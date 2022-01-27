2ee05cc5951f70f7b65cf95df9790cf7471e406d
Zavim <Zark471@gmail.com>
2022-01-27 13:39:50 -0500

    -chore remove old components, move README

--- react-facts/src/index.js
+++ react-facts/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
 import App from './App';
 import './style.css';
 
